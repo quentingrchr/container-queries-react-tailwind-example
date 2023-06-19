8d04714540c5fab680b9de58ca08d484cfe687c5
quentingrchr <q.grancher@gmail.com>
2023-06-19 17:25:00 +0200

    improve responsive

--- app/page.tsx
+++ app/page.tsx
@@ -3,13 +3,17 @@ import Image from "next/image";
 import articles from "./data";
 export default function Home() {
   return (
-    <main>
-      <div className="grid-custom p-10 sm:p-5">
+    <main className="flex flex-col">
+      <header className="flex flex-col gap-5 h-10 w-full bg-gray-200"></header>
+      <h1 className="text-2xl md:text-4xl font-bold text-center mt-10 mb-5">
+        Container Queries with React and Tailwind CSS
+      </h1>
+      <div className="grid-custom p-10 sm:p-5 ">
         {articles.map((article) => (
           <Article key={article.id} {...article} className="@container child" />
         ))}
         <aside className="sidebar bg-teal-500 p-5 text-xl font-bold">
-          {"I'm a sidebar"}
+          {"I'm an additional sidebar"}
         </aside>
       </div>
     </main>
